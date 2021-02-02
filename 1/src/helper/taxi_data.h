2e3e43fcd92c16fc73a8d68ba85c1e9b1e434708
dazzlemon <daanill13470@gmail.com>
2021-02-02 08:48:29 +0200

    upd

--- 1/src/helper/taxi_data.h
+++ 1/src/helper/taxi_data.h
@@ -1,43 +1,58 @@
 #include <random>
 #include <limits.h>
 
-auto taxi_station_data(const int&) -> std::pair<int**, int>;
-auto taxi_station_data() -> std::pair<int**, int>;
-int* taxi_data(const int& seed);
+auto taxi_station_data(int) -> std::pair<int**, size_t>;
+auto taxi_station_data() -> std::pair<int**, size_t>;
 void delete_taxi_station_data(int**, int);
 
-auto taxi_station_data() -> std::pair<int**, int> {
+/* in:
+ *   -
+ * out:
+ *   pair:
+ *     first - 2d array, 7 cols
+ *     second - amount of rows in "first"
+ * info:
+ *   generates n * 7 2d array,
+ *   n = 10 to 20,
+ *   fills it with values from 100 to 999
+ *   returns this array paired with n
+ *      actually delegates work to taxi_station(int) */
+auto taxi_station_data() -> std::pair<int**, size_t> {
 	auto rd = std::random_device();
 	return taxi_station_data(rd());
 }
 
-auto taxi_station_data(const int& seed) -> std::pair<int**, int> {
+/* in:
+ *   seed - number to init generator
+ * out:
+ *   same as taxi_station()
+ * info:
+ *   same as taxi_station() */
+auto taxi_station_data(int seed) -> std::pair<int**, size_t> {
 	auto generator = std::default_random_engine(seed);
-	auto distributionN = std::uniform_int_distribution<int>(10, 20);
-	auto distributionSeed = std::uniform_int_distribution<int>(0, INT_MAX);
-	auto nOfTaxis = distributionN(generator);
+	auto distributionRowsAmount = std::uniform_int_distribution<int>(10, 20);
+	auto distributionDay = std::uniform_int_distribution<int>(100, 999);
+	
+	size_t nOfTaxis = distributionRowsAmount(generator);
 	auto data_ = new int*[nOfTaxis];
-	for (int i = 0; i < nOfTaxis; i++) {
-		auto taxi_seed = distributionSeed(generator);
-		auto row = taxi_data(taxi_seed);
-		data_[i] = row;
+	for (size_t i = 0; i < nOfTaxis; i++) {
+		data_[i] = new int[7];
+		for (int j = 0; j < 7; j++)
+			data_[i][j] = distributionDay(generator);
 	}
 	return std::make_pair(data_, nOfTaxis);
 }
 
-int* taxi_data(const int& seed) {
-	auto generator = std::default_random_engine(seed);
-	auto distribution = std::uniform_int_distribution<int>(100, 999);
-	auto data_ = new int[7];
-	for (int i = 0; i < 7; i++)
-		data_[i] = distribution(generator);
-	return data_;
-}
-
-void delete_taxi_station_data(int** data_, int size) {
-	for (int i = 0; i < size; i++)
+/* in:
+ *   data_ - 2d array n * 7
+ *   size - n for "data_"
+ * out:
+ *   -
+ * info:
+ *   frees memory of "data_"
+ * */
+void delete_taxi_station_data(int** data_, size_t size) {
+	for (size_t i = 0; i < size; i++)
 		delete [] data_[i];
 	delete [] data_;
 }
-
-
