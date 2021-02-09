5960c58424928dbea65c31783e83eef6d26eff10
dazzlemon <daanill13470@gmail.com>
2021-02-09 18:27:18 +0200

    better matplotlib plot

--- 2/src/main.py
+++ 2/src/main.py
@@ -11,11 +11,20 @@ with open('tests.txt', 'r') as f:
         f.readlines()
     )
 
-for s in data:
-    lis = list(s)
-    print(lis)
-    plt.scatter(y=lis,
-                x=np.arange(0, len(lis)))
-plt.legend(['quicksort', 'bubblesort', 'gnomesort'])
+labels = ['quicksort', 'bubblesort', 'gnomesort']
+for s, l in zip(data, labels):
+    y = list(s)
+    x = np.arange(0, len(y))
 
+    z = np.polyfit(x, y, 2)
+    p = np.poly1d(z)
+
+    plt.scatter(x, y,
+                label=l + ' y=%.6fx2+(%.6fx)+(%.6f)'%(z[0], z[1], z[2]))
+    #plt.plot(x, p(x), 'r--')
+
+plt.legend(loc='upper left')
+plt.title('Sorting algorithms')
+plt.xlabel('Length of array')
+plt.ylabel('Operations taken')
 plt.show()
