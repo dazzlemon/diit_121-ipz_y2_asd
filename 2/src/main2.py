604cb02b29e256662365065644d0e393a3d3e6ff
dazzlemon <daanill13470@gmail.com>
2021-02-09 21:11:01 +0200

    java x python

--- 2/src/main2.py
+++ 2/src/main2.py
@@ -1,21 +1,14 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 from py4j.java_gateway import JavaGateway
 
 gateway = JavaGateway()
 
-data = [
-    gateway.jvm.src.Tests.quickSort(),
-    gateway.jvm.src.Tests.bubbleSort(),
-    gateway.jvm.src.Tests.gnomeSort(),
-]
-
-data[0] = (data[0], 'quicksort 10 * n * log(n)', lambda n: 10 * n * np.log(n))
-data[1] = (data[1], 'bubblesort 3.5 * n^2', lambda n: 3.5 * n * n)
-data[2] = (data[2], 'gnomesort 1.75 * n^2', lambda n: 1.75 * n * n)
-
-data = np.array(data, dtype=object)
+data = np.array([
+    (list(gateway.jvm.src.Tests.quickSort()), 'quicksort 10 * n * log(n)', lambda n: 10 * n * np.log(n)),
+    (list(gateway.jvm.src.Tests.bubbleSort()), 'bubblesort 3.5 * n^2', lambda n: 3.5 * n * n),
+    (list(gateway.jvm.src.Tests.gnomeSort()), 'gnomesort 1.75 * n^2', lambda n: 1.75 * n * n)
+])
 
 for s, l, f in data[[0, 1, 2]]:
     x = np.arange(0, len(s))
