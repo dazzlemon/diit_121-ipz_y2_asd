3276a4b023c1976fa9820bdce4bc8e749e3c7741
dazzlemon <daanill13470@gmail.com>
2021-02-10 15:22:37 +0200

    small refactor in main2.py

--- 2/src/main2.py
+++ 2/src/main2.py
@@ -5,22 +5,22 @@ from scipy.optimize import curve_fit
 
 gateway = JavaGateway()
 
+# [1:] is to skip the first element which is zero
 data = np.array([
-    (np.array(gateway.jvm.src.Tests.quickSort()), 'quicksort; %5.3f * n * log(n)', lambda n, c: c * n * np.log(n)),
-    (np.array(gateway.jvm.src.Tests.bubbleSort()), 'bubblesort; %5.3f * n^2', lambda n, c: c * n * n),
-    (np.array(gateway.jvm.src.Tests.gnomeSort()), 'gnomesort; %5.3f * n^2', lambda n, c: c * n * n)
+    (np.array(gateway.jvm.src.Tests.quickSort())[1:], 'quicksort; %5.3f * n * log(n)', lambda n, c: c * n * np.log(n)),
+    (np.array(gateway.jvm.src.Tests.bubbleSort())[1:], 'bubblesort; %5.3f * n^2', lambda n, c: c * n * n),
+    (np.array(gateway.jvm.src.Tests.gnomeSort())[1:], 'gnomesort; %5.3f * n^2', lambda n, c: c * n * n)
 ], dtype=object)
-
+ 
 for s, l, f in data[[0, 1, 2]]:
-    x = np.arange(0, len(s))
-    fitargs, cov = curve_fit(f, x[1:], s[1:])
-
-    fitdata = f(x[1:], *fitargs)
-    abs_err = s[1:] - fitdata
+    x = np.arange(1, len(s) + 1)
+    fitargs, cov = curve_fit(f, x, s)
+    fitdata = f(x, *fitargs)
+    abs_err = s - fitdata
     r2      = 1.0 - (np.var(abs_err) / np.var(fitdata))
 
     plt.scatter(x, s, label=l % tuple(fitargs) + '; R2 = %5.3f' % r2)
-    plt.plot(x[1:], fitdata, 'r--')
+    plt.plot(x, fitdata, 'r--')
 
 plt.legend(loc='upper left')
 plt.title('Sorting algorithms')
