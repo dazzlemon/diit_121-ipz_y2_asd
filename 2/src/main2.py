860c3ce5da7b3c1511d76e42689e319ce7886f80
dazzlemon <daanill13470@gmail.com>
2021-02-10 15:13:18 +0200

    correct constant and r2 for all algos

--- 2/src/main2.py
+++ 2/src/main2.py
@@ -6,25 +6,21 @@ from scipy.optimize import curve_fit
 gateway = JavaGateway()
 
 data = np.array([
-    (list(gateway.jvm.src.Tests.quickSort())[1:], 'quicksort %5.3f * n * log(n)', lambda n, c: c * n * np.log(n)),
-    (list(gateway.jvm.src.Tests.bubbleSort()), 'bubblesort %5.3f * n^2', lambda n, c: c * n * n),
-    (list(gateway.jvm.src.Tests.gnomeSort()), 'gnomesort %5.3f * n^2', lambda n, c: c * n * n)
+    (np.array(gateway.jvm.src.Tests.quickSort()), 'quicksort; %5.3f * n * log(n)', lambda n, c: c * n * np.log(n)),
+    (np.array(gateway.jvm.src.Tests.bubbleSort()), 'bubblesort; %5.3f * n^2', lambda n, c: c * n * n),
+    (np.array(gateway.jvm.src.Tests.gnomeSort()), 'gnomesort; %5.3f * n^2', lambda n, c: c * n * n)
 ], dtype=object)
 
 for s, l, f in data[[0, 1, 2]]:
     x = np.arange(0, len(s))
-    fitargs, cov = curve_fit(f, x, s)
+    fitargs, cov = curve_fit(f, x[1:], s[1:])
 
-    fitdata = f(x, *fitargs)
-    abs_err = s - fitdata
+    fitdata = f(x[1:], *fitargs)
+    abs_err = s[1:] - fitdata
+    r2      = 1.0 - (np.var(abs_err) / np.var(fitdata))
 
-    sqr_err           = np.square(abs_err)
-    mean_sqr_err      = np.mean(sqr_err)
-    root_mean_sqr_err = np.sqrt(mean_sqr_err)
-    r2                = 1.0 - (np.var(abs_err) / np.var(fitdata))
-
-    plt.scatter(x, s, label=l % tuple(fitargs) + ' R2 = %5.3f' % r2)
-    plt.plot(x, fitdata, 'r--')
+    plt.scatter(x, s, label=l % tuple(fitargs) + '; R2 = %5.3f' % r2)
+    plt.plot(x[1:], fitdata, 'r--')
 
 plt.legend(loc='upper left')
 plt.title('Sorting algorithms')
