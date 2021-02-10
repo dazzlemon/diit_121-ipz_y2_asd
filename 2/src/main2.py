05aafad36fa671dfc5a1227b43d22045540b262b
dazzlemon <daanill13470@gmail.com>
2021-02-10 14:24:08 +0200

    curve fit

--- 2/src/main2.py
+++ 2/src/main2.py
@@ -1,19 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from py4j.java_gateway import JavaGateway
+from scipy.optimize import curve_fit
 
 gateway = JavaGateway()
 
 data = np.array([
-    (list(gateway.jvm.src.Tests.quickSort()), 'quicksort 10 * n * log(n)', lambda n: 10 * n * np.log(n)),
-    (list(gateway.jvm.src.Tests.bubbleSort()), 'bubblesort 3.5 * n^2', lambda n: 3.5 * n * n),
-    (list(gateway.jvm.src.Tests.gnomeSort()), 'gnomesort 1.75 * n^2', lambda n: 1.75 * n * n)
-])
+    (list(gateway.jvm.src.Tests.quickSort())[1:], 'quicksort %5.3f * n * log(n)', lambda n, c: c * n * np.log(n)),
+    (list(gateway.jvm.src.Tests.bubbleSort()), 'bubblesort %5.3f * n^2', lambda n, c: c * n * n),
+    (list(gateway.jvm.src.Tests.gnomeSort()), 'gnomesort %5.3f * n^2', lambda n, c: c * n * n)
+], dtype=object)
 
 for s, l, f in data[[0, 1, 2]]:
     x = np.arange(0, len(s))
-    plt.scatter(x, s, label=l)
-    plt.plot(x, f(x), 'r--')
+    popt, pcov = curve_fit(f, x, s)
+    plt.scatter(x, s, label=l % tuple(popt))
+    plt.plot(x, f(x, *popt), 'r--')
 
 plt.legend(loc='upper left')
 plt.title('Sorting algorithms')
