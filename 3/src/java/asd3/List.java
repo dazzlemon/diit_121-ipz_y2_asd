31aa47448750ac8412a852b7262abcc0e29be75c
dazzlemon <daanill13470@gmail.com>
2021-02-18 19:42:28 +0200

    added PriorityQueue & List

---
+++ 3/src/java/asd3/List.java
@@ -0,0 +1,27 @@
+package asd3;
+
+public class List <E> {
+	public boolean isEmpty() {
+
+	}
+
+	
+	public int size() {
+
+	}
+
+
+	public void add(int index, E e) {
+
+	}
+
+
+	public E get(int index) {
+
+	}
+
+
+	public E remove(int index) {
+
+	}
+}
---
+++ 3/src/java/asd3/PriorityQueue.java
@@ -0,0 +1,29 @@
+package asd3;
+
+public class PriorityQueue <E extends Comparable<E>> {
+	private List<E> list;	
+
+
+	public boolean isEmpty() {
+		return list.isEmpty();	
+	}
+
+
+	public void add(E e) {
+		for (int i = 0; i < list.size(); i++) {
+			if (e.compareTo(list[i]) < 0) {
+				list.add(i, e);
+			}
+		}
+	}
+
+
+	public E peek() {
+		return list.get(list.size() - 1);
+	}
+
+
+	public E poll() {
+		return list.remove(list.size() - 1);
+	}
+}
