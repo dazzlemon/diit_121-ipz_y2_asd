7b321d8a6dfb9143f3b1bf0db341be353af85cf4
dazzlemon <daanill13470@gmail.com>
2021-02-18 22:08:37 +0200

    half done iterators & list

--- 3/src/java/asd3/main/List.java
+++ 3/src/java/asd3/main/List.java
@@ -1,18 +1,53 @@
 package asd3.main;
 
 public class List <E> {
-	public boolean isEmpty() {
+	private class Node {
+		public Node next;
+		public E data;
+	}
 
+
+	private class Iterator {
+		Node next;
+
+
+		public Iterator(Node n) {
+			this.next = n;
+		}
+		
+
+		public boolean hasNext() {
+			return next != null;
+		}
+
+		
+		public E next() {
+			E e = next.data;
+			next = next.next;
+			return e;
+		}
+	}
+
+
+	private Node head = null;
+
+
+	public boolean isEmpty() {
+		return head == null;	
 	}
 
 	
 	public int size() {
-
+		int size = 0;
+		for (var i : Iterator(head)) {
+			size++;
+		}
+		return size;
 	}
 
 
 	public void add(int index, E e) {
-
+		
 	}
 
 
