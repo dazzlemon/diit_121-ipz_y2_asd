790dd0b9d4c728b00919255b985e5a7dc3a97c9a
dazzlemon <daanill13470@gmail.com>
2021-02-19 17:46:10 +0200

    add at end tested

--- 3/src/java/asd3/test/ListTest.java
+++ 3/src/java/asd3/test/ListTest.java
@@ -4,18 +4,47 @@ import asd3.main.List;
 
 class ListTest implements ITest {
 	public void execute() {
+		this.isEmptyTest();	
+		this.sizeTest();
+		this.addTest();
+		this.addIdxTest();
+	}
+
+
+	public void isEmptyTest() {
 		var list = new List<Integer>();
 		assert list.isEmpty(): "Test not passed: List::isEmpty() -> true";
 
-
 		list.add(0, 0);
 		assert list.isEmpty() == false: "Test not passed: List::isEmpty() -> false";
-		list = new List<Integer>();
+	}
 
 
+	public void sizeTest() {
+		var list = new List<Integer>();
 		for (int i = 0; i < 10; i++) {
 			list.add(i, i);
 			assert list.size() == (i + 1): "Test not passed: List::size() -> " + (i + 1);
 		}
+	}
+
+
+	public void addTest() {
+		var list = new List<Integer>();
+		var str  = "";
+		for (int i = 0; i < 10; i++) {
+			list.add(i, i);
+			str += i;
+			var str_ = "";
+			for (int j = 0; j < list.size(); j++) {
+				str_ += list.get(j);
+			}
+			assert str.compareTo(str_) == 0: "Test not passed: List::add -> Expected: " + str + ", Actual: " + str_; 
+		}
+	}
+
+
+	public void addIdxTest() {
+
 	}
 }
