9d7b14b9d5ec5320e2d4fab4bff48a364ee67b40
dazzlemon <daanill13470@gmail.com>
2021-04-09 22:03:53 +0300

    shorter test

--- 5/src/main/java/asd5/BstTest.java
+++ 5/src/main/java/asd5/BstTest.java
@@ -1,5 +1,8 @@
 package asd5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BstTest {
     public static void bstTest() {
         var bst = new BinarySearchTree<Integer, Integer>();
@@ -19,7 +22,8 @@ public class BstTest {
         for (var i : bst) {
             System.out.print(i + " ");
         }
-        System.out.println("expected 0 1 2 3 4 5 6 7 8 9");
+        System.out.println();
+        System.out.println("0 1 2 3 4 5 6 7 8 9 expected");
 
         // 2
         bst.add(6, 6);
@@ -40,83 +44,22 @@ public class BstTest {
         System.out.println("0 1 2 3 4 5 6 7 8 9 expected");
 
         // 3
-        bst.poll(8);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("0 1 2 3 4 5 6 7 9 expected (8)");
+        var removeOrder = Arrays.asList(8, 4, 1, 2, 7, 0, 3, 9, 6, 5);
+        var ord = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-        // 4
-        bst.poll(4);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("0 1 2 3 5 6 7 9 expected (4)");
+        for (var i : removeOrder) {
+            bst.poll(i);
+            ord.remove(i);
 
-        // 5
-        bst.poll(1);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("0 2 3 5 6 7 9 expected (1)");
+            for (var j : bst) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
 
-        // 6
-        bst.poll(2);
-        for (var i : bst) {
-            System.out.print(i + " ");
+            for (var j : ord) {
+                System.out.print(j + " ");
+            }
+            System.out.println("expected (" + i + ")");
         }
-        System.out.println();
-        System.out.println("0 3 5 6 7 9 expected (2)");
-
-        // 7
-        bst.poll(7);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("0 3 5 6 9 expected (7)");
-
-        // 8
-        bst.poll(0);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("3 5 6 9 expected (0)");
-
-        // 9
-        bst.poll(3);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("5 6 9 expected (3)");
-
-        // 10
-        bst.poll(9);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("5 6 expected (9)");
-
-        // 11
-        bst.poll(6);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("5 expected (6)");
-
-        // 11
-        bst.poll(5);
-        for (var i : bst) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        System.out.println("expected none (5)");
     }
 }
