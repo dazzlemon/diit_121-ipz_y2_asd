522a1f35b15465ce5e6d7068f6fc6ad764ba96c1
dazzlemon <daanill13470@gmail.com>
2021-05-02 04:50:00 +0300

    class TreeSetIndexed

--- 6/src/main/java/asd6/AdjacencyMatrix.java
+++ 6/src/main/java/asd6/AdjacencyMatrix.java
@@ -1,14 +1,39 @@
 package asd6;
 
 import java.util.LinkedList;
+import java.util.TreeSet;
 
 public class AdjacencyMatrix implements Graph {
     LinkedList<LinkedList<Boolean>> matrix;
-    LinkedList<String> ids;
+    TreeSetIndexed<String> ids;
     
+    private class TreeSetIndexed <T extends Comparable<T>> extends TreeSet <T> {
+        public T get(int i) {
+            int j = 0;
+            for (var e : this) {
+            if (i == j) {
+                return e;
+            }
+            j++;
+            }
+            return null;
+        }
+
+        public int indexOf(T t) {
+            int i = 0;
+            for (var e : this) {
+            if (e.compareTo(t) == 0) {
+                return i;
+            }
+            i++;
+            }
+            return -1;
+        }
+    }
+
     AdjacencyMatrix() {
         matrix = new LinkedList<>();
-        ids    = new LinkedList<>();
+        ids    = new TreeSetIndexed<>();
     }
     
     @Override
