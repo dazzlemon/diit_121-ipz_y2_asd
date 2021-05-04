bd9ff4355332a41199ad1585b2004b03e97c4784
dazzlemon <daanill13470@gmail.com>
2021-05-04 18:15:32 +0300

    adjacency matrix seems done(at least add funcs)

--- 6/src/main/java/asd6/AdjacencyMatrix.java
+++ 6/src/main/java/asd6/AdjacencyMatrix.java
@@ -7,14 +7,46 @@ public class AdjacencyMatrix extends AbstractGraph {
     LinkedList<LinkedList<Boolean>> matrix;
     TreeSetIndexed<String> ids;
     
+    private class Vertex extends AbstractGraph.Vertex {
+        int i;
+
+        public Vertex(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public String getId() {
+            return ids.get(i);
+        }
+
+        @Override
+        public Iterable<asd6.AbstractGraph.Vertex> neighbours() {
+            var list = new LinkedList<AbstractGraph.Vertex>();
+            for (int j = 0; j < ids.size(); j++) {
+                if (matrix.get(i).get(j)) {
+                    list.add(new Vertex(j));
+                }
+            }
+            return list;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other.getClass() != this.getClass()) {
+                return false;
+            }
+            return i == ((Vertex)other).i;
+        }
+    }
+
     private class TreeSetIndexed <T extends Comparable<T>> extends TreeSet <T> {
         public T get(int i) {
             int j = 0;
             for (var e : this) {
-            if (i == j) {
-                return e;
-            }
-            j++;
+                if (i == j) {
+                    return e;
+                }
+                j++;
             }
             return null;
         }
@@ -22,10 +54,10 @@ public class AdjacencyMatrix extends AbstractGraph {
         public int indexOf(T t) {
             int i = 0;
             for (var e : this) {
-            if (e.compareTo(t) == 0) {
-                return i;
-            }
-            i++;
+                if (e.compareTo(t) == 0) {
+                    return i;
+                }
+                i++;
             }
             return -1;
         }
@@ -39,34 +71,60 @@ public class AdjacencyMatrix extends AbstractGraph {
     @Override
     public void add(String v) {
         if (!ids.contains(v)) {
-            // TODO
+            ids.add(v);
+            var i = ids.indexOf(v);
+
+            matrix.add(i, new LinkedList<>());// new row
+            
+            for (int col = 0; col < ids.size() - 1; col++) {// -1 to set column later
+                matrix.get(i).add(false);// no edges at first
+            }
+
+            for (int row = 0; row < ids.size(); row++) {// add new column to each row
+                matrix.get(row).add(i, false);// no edges at first
+            }
         }
     }
 
     @Override
     public void add(String v1, String v2) {
-        if (ids.contains(v1) && ids.contains(v2)) {
-            // TODO
+        if (ids.contains(v1) && ids.contains(v2) && v1.compareTo(v2) != 0) {
+            var i1 = ids.indexOf(v1);
+            var i2 = ids.indexOf(v2);
+            
+            matrix.get(i1).set(i2, true);
         }
     }
 
     @Override
     public void remove(String v) {
         if (ids.contains(v)) {
-            // TODO
+            var i = ids.indexOf(v);
+
+            matrix.remove(i);// remove row
+            ids.remove(v);// remove id
+
+            for (int row = 0; row < ids.size(); row++) {
+                matrix.get(row).remove(i);// remove column
+            }
         }
     }
 
     @Override
     public void remove(String v1, String v2) {
         if (ids.contains(v1) && ids.contains(v2)) {
-            // TODO
+            var i1 = ids.indexOf(v1);
+            var i2 = ids.indexOf(v2);
+            
+            matrix.get(i1).set(i2, false);
         }
     }
 
     @Override
     protected Vertex stringToVertex(String v) {
-        // TODO
-        return null;
+        if (!ids.contains(v)) {
+            return null;
+        }
+        return new Vertex(ids.indexOf(v));
     }
 }
--- 6/src/main/java/asd6/App.java
+++ 6/src/main/java/asd6/App.java
@@ -2,6 +2,27 @@ package asd6;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var g = new AdjacencyMatrix();
+
+        g.add("A");
+        g.add("B");
+        g.add("C");
+        g.add("D");
+        g.add("E");
+
+        g.add("A", "B");
+
+        g.add("B", "A");
+        g.add("B", "C");
+
+        g.add("C", "A");
+        g.add("C", "E");
+        g.add("C", "D");
+
+        g.add("E", "A");
+
+        for (var v : g.dfs("A")) {
+            System.out.println(v);
+        }
     }
 }
